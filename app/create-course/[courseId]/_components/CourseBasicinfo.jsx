159c004cdc6419e1f9203bb57e755873b8fcfafd
hp1 <kushchandak04@gmail.com>
2024-11-26 01:09:33 +0530

    chapter details

---
+++ app/create-course/[courseId]/_components/CourseBasicinfo.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import Image from "next/image";
+import { HiOutlinePuzzle } from "react-icons/hi";
+
+function CourseBasicInfo({ course }) {
+  return (
+    <div className="border p-6 rounded-lg shadow-md mt-6 bg-white">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <div>
+          <h2 className="font-bold text-3xl text-primary">
+            {course?.courseOutput?.course?.name}
+          </h2>
+          <p className="text-sm text bg-white mt-4">
+            {course?.courseOutput?.course?.description}
+          </p>
+          <h2 className="flex items-center gap-2 mt-4 text-purple-600" ><HiOutlinePuzzle/>Health</h2>
+          <button className="bg-primary  text-purple-600 py-2 px-6 rounded mt-6">
+            Start
+          </button>
+        </div>
+        <div>
+          <Image
+            src={"/place.png"}
+            alt="Course Image"
+            width={100}
+            height={100}
+            className="w-full h-[300px] object-cover rounded-lg"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CourseBasicInfo;
+
+// import React from "react";
+// import Image from "next/image";
+// import { HiOutlinePuzzle } from "react-icons/hi";
+
+// function CourseBasicInfo({course}){
+//     return(
+//         <div className='p-10 border rounded-xl shadow-sm mt-5'>
+//             <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
+//                 <div>
+//                     <h2 className="font-bold text-3xl">{course?.courseOutput?.course?.name}</h2>
+//                     <p className="text-sm text-gray-400 mt-3 ">{course?.courseOutput?.course?.description}</p>
+//                     <h2 className='font-medium mt-2 flex gap-2 items-center text-primary'>Health</h2>
+//                     <button className="w-full mt-5" >Start</button>
+//                 </div>
+//                 <div>
+//                     <Image src={'/place.png'} width={50} height={100}
+//                     className='w-full round-xl h-[300px] object-cover'/>
+//                 </div>
+//             </div>
+            
+//         </div>
+//     )
+// }
+
+// export default CourseBasicInfo
--- app/create-course/[courseId]/page.jsx
+++ app/create-course/[courseId]/page.jsx
@@ -1,31 +1,101 @@
-"use client"
+"use client";
 
-import { CourseList } from '@/configs/schema'
-import { useUser } from '@clerk/nextjs'
-import { db } from '@/configs/db';
-import React , {useEffect} from 'react'
-import {and , eq} from 'drizzle-orm'
-
-function CourseLayout({params}) {
-    const {user} = useUser();
-    useEffect(() =>{
-        params&&GetCourse();
-    },[params,user])
+import { CourseList } from "@/configs/schema";
+import { useUser } from "@clerk/nextjs";
+import { db } from "@/configs/db";
+import React, { useEffect, useState } from "react";
+import { and, eq } from "drizzle-orm";
+import CourseBasicInfo from "./_components/CourseBasicinfo";
+import CourseDetail from "./_components/CourseDetail";
+import ChapterList from "./_components/ChapterList";
 
 
-    const GetCourse = async () =>{
-        const result=await db.select().from(CourseList)
-        .where(and(eq(CourseList.courseId,params?.courseId),
-        eq(CourseList?.createdBy,user?.primaryEmailAddress?.emailAddress)))
+function CourseLayout({ params }) {
+  const { user } = useUser();
+  const [course, setCourse] = useState(null);
 
-        console.log(result);
-    }
+  useEffect(() => {
+    if (params) GetCourse();
+  }, [params, user]);
+
+  const GetCourse = async () => {
+    const result = await db
+      .select()
+      .from(CourseList)
+      .where(
+        and(
+          eq(CourseList.courseId, params?.courseId),
+          eq(CourseList?.createdBy, user?.primaryEmailAddress?.emailAddress)
+        )
+      );
+    setCourse(result[0]);
+  };
 
   return (
-    <div>
-      CourseLayout
+    <div className="mt-10 px-5 md:px-16 lg:px-32">
+      <h2 className="font-bold text-center text-2xl text-purple-500">
+        Course Layout
+      </h2>
+      {/* Basic Info */}
+      <CourseBasicInfo course={course} />
+
+      {/* Course Details */}
+      <CourseDetail course={course} />
+
+      {/* List of Lessons */}
+      <ChapterList course={course} />
     </div>
-  )
+  );
 }
 
-export default CourseLayout
+export default CourseLayout;
+
+// "use client"
+
+// import { CourseList } from '@/configs/schema'
+// import { useUser } from '@clerk/nextjs'
+// import { db } from '@/configs/db';
+// import React , {useEffect, useState} from 'react'
+// import {and , eq} from 'drizzle-orm'
+// import CourseBasicInfo from './_components/CourseBasicinfo';
+// import CourseDetail from './_components/CourseDetail';
+// import ChapterList from './_components/ChapterList';
+
+
+
+
+// function CourseLayout({params}) {
+//     const {user} = useUser();
+//     const [course, setCourse] = useState([]);
+//     useEffect(() =>{
+//         params && GetCourse();
+//     }, [params,user])
+
+
+//     const GetCourse = async () =>{
+      
+//         const result=await db.select().from(CourseList)
+//         .where(and(eq(CourseList.courseId,params?.courseId),
+//         eq(CourseList?.createdBy,user?.primaryEmailAddress?.emailAddress)))
+//         setCourse(result[0]);
+//         console.log(result);
+//     }
+
+//   return (
+//     <div className='mt-10 px-7 md:px-20 lg:px-44'>
+//       <h2 className='font-bold text-center text-2xl'>Course Layout</h2>
+
+//       {/* Basic Info */}
+//          <CourseBasicInfo course={course} />
+
+//       {/* Course Details */}
+//         <CourseDetail course={course}/>
+//       {/* List of lessons */}
+//        <ChapterList course={course}/>
+
+//     </div>
+//   )
+// }
+
+// export default CourseLayout
+
